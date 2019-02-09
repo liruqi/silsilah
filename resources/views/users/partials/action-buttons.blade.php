a6c3d524ffa295c1928cfacdd42ed2eb9cd27b70
ruqli <ruqli@outlook.com>
2019-02-09 18:52:54 +0800

    Hide tree view if no child

--- resources/views/users/partials/action-buttons.blade.php
+++ resources/views/users/partials/action-buttons.blade.php
@@ -4,6 +4,8 @@
     @endcan
     {{ link_to_route('users.show', trans('app.show_profile').' '.$user->name, [$user->id], ['class' => Request::segment(3) == null ? 'btn btn-default active' : 'btn btn-default']) }}
     {{ link_to_route('users.chart', trans('app.show_family_chart'), [$user->id], ['class' => Request::segment(3) == 'chart' ? 'btn btn-default active' : 'btn btn-default']) }}
+    @if ($user->childs->count() > 0)
     {{ link_to_route('users.tree', trans('app.show_family_tree'), [$user->id], ['class' => Request::segment(3) == 'tree' ? 'btn btn-default active' : 'btn btn-default']) }}
+    @endif
     {{ link_to_route('users.marriages', trans('app.show_marriages'), [$user->id], ['class' => Request::segment(3) == 'marriages' ? 'btn btn-default active' : 'btn btn-default']) }}
 </div>
